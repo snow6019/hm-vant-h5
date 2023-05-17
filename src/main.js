cff59e491225b8aba984ad585b5f94577883834c
wsn <123@qq.com>
2023-05-17 12:45:24 +0800

    vant按需加载

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { Button, Icon } from 'vant'
+
+Vue.use(Button)
+Vue.use(Icon)
+
 Vue.config.productionTip = false
 
 new Vue({
